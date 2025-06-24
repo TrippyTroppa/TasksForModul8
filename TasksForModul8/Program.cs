2c1b575aabe7d9f65f73ec1f14691554fc94c0ff
Egor <egork2205@gmail.com>
2025-06-24 13:02:03 +0900

    Добавьте файлы проекта.

---
+++ TasksForModul8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksForModul8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Disc
+    {
+        public Disc(string name, long space, long freeSpace) 
+        {
+            Name = name;
+            Space = space;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public long Space { get; }
+        public long FreeSpace { get; }
+    }
+}
