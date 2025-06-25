d0b37ab8dddecd4201f7fe90f2b1ad97a70c2528
Egor <egork2205@gmail.com>
2025-06-25 21:08:58 +0900

    Задание 8.2.1

--- TasksForModul8/Program.cs
+++ TasksForModul8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -7,37 +8,41 @@ using System.Threading.Tasks;
 
 namespace TasksForModul8
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            GetCatalogs(); //   Вызов метода получения директорий
+        }
+
+        static void GetCatalogs()
+        {
+            string dirName = @"\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+                int folderCount = dirs.Length;
+
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+                int fileCount = files.Length;
+
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+
+                Console.WriteLine();
+                Console.WriteLine($"Итого объектов: {folderCount + fileCount}");
+                Console.WriteLine($"Из них:");
+                Console.WriteLine($"- Папок: {folderCount}");
+                Console.WriteLine($"- Файлов: {fileCount}");
+            }
         }
     }
-
-    public class Drive
-    {
-        public Drive(string name, long space, long freeSpace)
-        {
-            Name = name;
-            Space = space;
-            FreeSpace = freeSpace;
-        }
-
-        public string Name { get; }
-        public long Space { get; }
-        public long FreeSpace { get; }
-    }
-
-    public class Folder
-    {
-        public List<string> Files { get; set; } = new List<string>();
-    
-
-     Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
-
-        public void CreateFolder(string name)
-        {
-            Folders.Add(name, new Folder());
-        }
-    } 
 }
+
+    
